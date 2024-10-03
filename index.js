17e504c3efdaee916f769f9b28f049fe2b13d5b2
jameskimani001 <jameskim092t@gmail.com>
2024-10-03 11:11:36 +0300

    scope completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; 
+}
+
+
+ // Declare a constant called leastFavoriteCustomer
+const leastFavoriteCustomer = 'some value'; // Assign an initial value
+
+// Function that attempts to change the leastFavoriteCustomer constant
+function changeLeastFavoriteCustomer() {
+    // This line will throw an error if uncommented
+    leastFavoriteCustomer = 'another value'; // This will cause an error
+}
